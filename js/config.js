c1db9d3d743389083aa12523f0c9b0f2ab9c53f5
AryanCodeMaster <aryan.shetty.26.2004@gmail.com>
2026-09-21 17:53:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_NOd15MYcd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '58i5uodsumvbm6vo9ruotak0os', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
